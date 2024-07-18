ac5e14dd0c0f8fa576e34ff2cd98e36f7a243bd9
ambreen zubari <ambreenzubari3ss@gmail.com>
2024-07-19 00:11:28 +0500

    added author and time and source

--- src/components/News.js
+++ src/components/News.js
@@ -8,7 +8,7 @@ export class News extends Component {
 
   static defaultProps = {
     country: "us",
-    pageSize: 10, 
+    pageSize: 10,
     category: "general",
   }
 
@@ -23,76 +23,99 @@ export class News extends Component {
     this.state = { articles: this.articles, loading: false, page: 1 };
   }
 
+
   async componentDidMount() {
-   this.getData(1)
+    this.getData(1)
   }
 
+  formatDateString = (dateString) => {
+    let givenDate = new Date(dateString);
+    let   today = new Date();
+    let yesterday = new Date();
+    yesterday.setDate(today.getDate() - 1);
+    let isToday = givenDate.toDateString() === today.toDateString();
+    let isYesterday = givenDate.toDateString() === yesterday.toDateString();
+
+    if(isToday) {
+      return "Today";
+    } else if(isYesterday) {
+      return "Yesterday";
+    } else {
+      return givenDate.toLocaleDateString(); // or any other date format you prefer
+    }
+  }
+
+
+
   getData = async (pageNum) => {
-    this.setState({loading:true, articles: []})
+    this.setState({ loading: true, articles: [] })
     let url =
       `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0c6e9cfd0e914b22be720b3bf2b6bddd&pageSize=${this.props.pageSize}&page=${pageNum}`;
 
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles ,totalArticles: parsedData.totalResults, loading:false});
+    this.setState({ articles: parsedData.articles, totalArticles: parsedData.totalResults, loading: false });
   };
   handlePrevClick = () => {
-    this.getData(this.state.page-1)
+    this.getData(this.state.page - 1)
     this.setState({ page: this.state.page - 1 });
   };
 
   handleNextClick = () => {
-  if(this.state.page+1>Math.ceil(this.state.totalArticles/20)){
+    if (this.state.page + 1 > Math.ceil(this.state.totalArticles / 20)) {
 
-  }else{
-    this.setState({loading:true})
-    this.getData(this.state.page+1)
-    this.setState({ page: this.state.page + 1 });
-  }
+    } else {
+      this.setState({ loading: true })
+      this.getData(this.state.page + 1)
+      this.setState({ page: this.state.page + 1 });
+    }
   };
   render() {
-      return (
-        
-        <div className="container my-3">
-          <h2 className="text-center">Top Headlines</h2>
-          {this.state.loading && <Spinner />}
-          {!this.state.loading && (
-            <div className="row row-cols-1 row-cols-md-3 g-4">
-              {this.state.articles.map((article, index) => (
-                <div className="col" key={index}>
-                  <NewsItem
-                    title={article.title}
-                    description={article.description}
-                    imgUrl={article.urlToImage || "https://about.fb.com/wp-content/uploads/2023/09/GettyImages-686732223.jpg"}
-                    newsUrl={article.url}
-                  />
-                </div>
-              ))}
-            </div>
-          )}
-          {!this.state.loading && (
-            <div className="container mt-3">
-              <div className="d-flex justify-content-between">
-                <button
-                  disabled={this.state.page <= 1}
-                  className="btn btn-dark"
-                  onClick={this.handlePrevClick}
-                >
-                  &larr; Previous
-                </button>
-                <button
-                  disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles / 20)}
-                  className="btn btn-dark"
-                  onClick={this.handleNextClick}
-                >
-                  Next &rarr;
-                </button>
+    return (
+
+      <div className="container my-3">
+        <h2 className="text-center">Top Headlines</h2>
+        {this.state.loading && <Spinner />}
+        {!this.state.loading && (
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {this.state.articles.map((article, index) => (
+              <div className="col" key={index}>
+                <NewsItem
+                  title={article.title}
+                  description={article.description}
+                  imgUrl={article.urlToImage || "https://about.fb.com/wp-content/uploads/2023/09/GettyImages-686732223.jpg"}
+                  newsUrl={article.url}
+                  source={article.source.name}
+                  author={!article.author ? 'Unknown' : article.author}
+                  date={this.formatDateString(article.publishedAt)}
+                />
               </div>
+            ))}
+          </div>
+        )}
+        {!this.state.loading && (
+          <div className="container mt-3">
+            <div className="d-flex justify-content-between">
+              <button
+                disabled={this.state.page <= 1}
+                className="btn btn-dark"
+                onClick={this.handlePrevClick}
+              >
+                &larr; Previous
+              </button>
+              <button
+                disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles / 20)}
+                className="btn btn-dark"
+                onClick={this.handleNextClick}
+              >
+                Next &rarr;
+              </button>
             </div>
-          )}
-        </div>
-      );
-    }
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import '../App.css'; // Ensure you import the CSS file
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imgUrl, newsUrl } = this.props;
+    let { title, description, imgUrl, newsUrl, author, date, source } = this.props;
     return (
-      <div className="card h-100 mt-1">
+      <div className="card h-100 mt-1" style={{ position: "relative" }}>
+        <div className="ribbon">
+          <span>{source}</span>
+        </div>
         <img src={imgUrl} className="card-img-top" alt="News Thumbnail" />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
@@ -12,10 +17,25 @@ export class NewsItem extends Component {
           <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-dark">
             Read More
           </a>
+          <p className="card-text">
+            <small className="text-muted">
+              By <b>{author}</b> on <b>{date}</b>
+            </small>
+          </p>
         </div>
       </div>
     );
   }
 }
+
+NewsItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string.isRequired,
+  newsUrl: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  source: PropTypes.string.isRequired,
+};
 
 export default NewsItem;
