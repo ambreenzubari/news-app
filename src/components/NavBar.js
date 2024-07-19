74d409caf7fc697d18f396f12815d820a577cde6
ambreen zubari <ambreenzubari3ss@gmail.com>
2024-07-19 13:23:59 +0500

    change the header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import { categories } from "../model/parameters";
-import {
-  Link
-} from "react-router-dom";
+import { categories, countries, languages } from "../model/parameters";
+import { Link } from "react-router-dom";
+import "../App.css"
 export class NavBar extends Component {
   render() {
     return (
@@ -38,6 +37,52 @@ export class NavBar extends Component {
                 </li>
               ))}
             </ul>
+           
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="#"
+                  id="languagesDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Languages
+                </Link>
+                <ul className="dropdown-menu dropdown-menu-end dropdown-custom" aria-labelledby="languagesDropdown">
+                  {languages.map((language, index) => (
+                    <li><Link className="dropdown-item" to="#">{language.name}</Link></li>
+                  ))}
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="#"
+                  id="countriesDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Countries
+                </Link>
+                <ul className="dropdown-menu dropdown-menu-end dropdown-custom" aria-labelledby="countriesDropdown">
+                  {countries.map((country, index) => (
+                    <li><Link className="dropdown-item" to="#">{country.name}</Link></li>
+                  ))}
+                </ul>
+              </li>
+            </ul>
+
+            <form className="d-flex ms-3">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+            </form>
           </div>
         </div>
       </nav>
